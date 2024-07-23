1a6e56844168649577c5b7b0cd2df36b69ddf305
ShadmanKhan10 <khanshadman7777@gmail.com>
2024-07-23 15:58:37 +0530

    learned display property of css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section className="section-style">
+        <h1>Learning CSS</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
